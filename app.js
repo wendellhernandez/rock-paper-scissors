3fa5a88ef475535871856e748793bfcbed782083
wendellhernandez <wendellhbusiness@gmail.com>
2023-12-19 16:41:55 +0800

    changed styles, added more enemy, added find search enemy button, added lvl and xp bar, used localStorage

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 const weaponBox = document.querySelector('.weaponBox');
 const enemyBox = document.querySelector('.enemyBox img');
+const enemyBoxH3 = document.querySelector('.enemyBox h3');
 const round = document.querySelector('.lifeBox h3');
 const lives = document.querySelector('.lives');
 const story = document.querySelector('.story');
-const main = document.querySelector('main');
+const body = document.querySelector('body');
+const reloadButton = document.querySelector('.reloadButton');
 
 let enemyWeapon = '';
+let enemyArray = ['Centaur', 'Dwarf', 'Elf', 'Treant', 'Giant', 'Goblin', 'Mermaid', 'Ogre', 'Orc', 'Troll', 'Barbarian', 'Caveman', 'Warrior Princess', 'Dark Lord', 'Dragonslayer', 'Fairy', 'Wizard', 'Witch', 'Dragon', 'Angel', 'Demon', 'Spirit', 'Werewolf', 'Ghost', 'Zombie', 'Vampire'];
+let enemyFound = enemyArray[randomNumber(0, enemyArray.length-1)];
 let myWeapon = '';
 let roundCount = 0;
-let myLifeCount = 5;
-let enemyLifeCount = 5;
+let myLifeCount = 2;
+let enemyLifeCount = randomNumber(2,9);
+let xpGain = randomNumber(57, 341);
+let xpLost = randomNumber(61, 171);
+let xpTotal = localStorage.getItem('xpTotal') || 0;
+let xpExcess = 0;
+let level = 1;
 
+function randomNumber(start, end){
+    return start + Math.round(Math.random()*(end-start));
+}
+
+enemyBoxH3.textContent = enemyFound;
 round.textContent = `Round: ${roundCount}`;
-lives.textContent = `Your Lives: ${myLifeCount} | Enemy's Lives: ${enemyLifeCount}`;
+lives.textContent = `Your HP: ${myLifeCount} | ${enemyFound}'s HP: ${enemyLifeCount}`;
 
 weaponBox.addEventListener('click' , e => {
-    pickMyWeapon(e);
-    pickEnemyWeapon(e);
-    startFight(e);
+    if(e.target !== weaponBox){
+        reloadButton.style.display = 'none';
+
+        pickMyWeapon(e);
+        pickEnemyWeapon(e);
+        startFight(e);
+    }
 })
 
 function pickMyWeapon(e){
-    if(e.target !== weaponBox){
         for(let i=0; i<weaponBox.children.length; i++){
             weaponBox.children[i].classList.remove('active');
         }
@@ -29,27 +46,23 @@ function pickMyWeapon(e){
         e.target.classList.add('active');
 
         myWeapon = e.target.id;
-    }
 }
 
 function pickEnemyWeapon(e){
-    if(e.target !== weaponBox){
-        const rand = Math.ceil(Math.random()*3);
+        const rand = randomNumber(1, 3);
         if(rand == 1) enemyWeapon = 'rock';
         else if(rand == 2) enemyWeapon = 'paper';
         else enemyWeapon = 'scissors';
 
         enemyBox.attributes.src.value = `assets/${enemyWeapon}.png`;
-    }
 }
 
 function startFight(e){
-    if(e.target !== weaponBox){
         roundCount++;
         round.textContent = `Round: ${roundCount}`;
 
         if(myWeapon == enemyWeapon){
-            story.textContent = `Two ${myWeapon} means a draw, so no lives were lost. Your will to fight gets stronger`;
+            story.textContent = `Two ${myWeapon} means a draw, so no lives were lost. You are getting ready to fight again`;
         }else if(myWeapon == 'rock'){
             analyzeFight('scissors');
         }else if(myWeapon == 'paper'){
@@ -57,7 +70,6 @@ function startFight(e){
         }else{
             analyzeFight('paper');
         }
-    }
 }
 
 function analyzeFight(enemWeapon){
@@ -68,10 +80,12 @@ function analyzeFight(enemWeapon){
         lives.textContent = `Your Lives: ${myLifeCount} | Enemy's Lives: ${enemyLifeCount}`;
 
         if(enemyLifeCount == 0){
-            main.innerHTML = `
-                <div class="resultsWin">
-                    You killed an ogre <br>+25xp            
-                </div>`;
+            xpTotal = Number(xpTotal) + xpGain;
+            localStorage.setItem('xpTotal' , xpTotal);
+            xpExcess = xpTotal%1000;
+            level = Math.floor(xpTotal/1000)+1;
+
+            body.innerHTML = setBodyInnerHtml(`You killed the ${enemyFound}`,'Win' , '+'+xpGain);
         }
     }else{
         story.textContent = `Unfortunate defeat.. You lost one life, because your ${myWeapon} lacks the power to defend against the enemy's ${enemyWeapon}!`;
@@ -80,14 +94,55 @@ function analyzeFight(enemWeapon){
         lives.textContent = `Your Lives: ${myLifeCount} | Enemy's Lives: ${enemyLifeCount}`;
 
         if(myLifeCount == 0){
-            main.innerHTML = `
-                <div class="results">
-                    You are dead            
-                </div>`;
+            xpTotal = Number(xpTotal) - xpLost;
+            if(xpTotal < 0) xpTotal = 0;
+            localStorage.setItem('xpTotal' , xpTotal);
+            xpExcess = xpTotal%1000;
+            level = Math.floor(xpTotal/1000)+1;
+
+            body.innerHTML = setBodyInnerHtml(`The ${enemyFound} defeated you`,'Lost' , '-'+xpLost);
         }
     }
 }
 
-function changeScreen(){
+function setBodyInnerHtml(message, result ,xp){
+    return `
+    <header>
+        <div class="container">
+            <h1>
+                ${message} <br><span id="result${result}">${xp}xp</span>
+            </h1>
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="result">
+                <div class="xpBox">
+                    <div class="lvl">lvl. ${level}</div>
+                    <div class="xpBar">
+                        <div class="xpLength" style="width: ${xpExcess/10}%"></div>
+                    </div>
+                    <div class="xpTotal">${xpExcess}/1000xp</div>
+                </div>
+                <button class="reloadButton" onclick="reloadPage();">
+                Search for a new enemy
+                </button>
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        Project by: <a href="https://github.com/wendellhernandez">Wendell Hernandez</a>
+        <br>
+        Tech Stack: <span>HTML, CSS, Javascript, LESS</span>
+        <br><br>
+        Copyright &copy; 2023
+    </footer>
     
+    <script src="app.js"></script>`
+}
+
+function reloadPage(){
+    location.reload();
 }
